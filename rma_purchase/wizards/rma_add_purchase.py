303c8c5d2eec35c0c025c853abd36b385dfa2ef1
aheficent <ahenriquez@eficent.com>
2017-05-10 11:30:18 +0200

    [FIX]wizards fill lines missed files

--- rma_purchase/wizards/rma_add_purchase.py
+++ rma_purchase/wizards/rma_add_purchase.py
@@ -12,13 +12,13 @@ from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as DT_FORMAT
 import openerp.addons.decimal_precision as dp
 
 
-class RmaAddinvoice(models.TransientModel):
-    _name = 'rma_add_invoice'
+class RmaAddPurchase(models.TransientModel):
+    _name = 'rma_add_purchase'
     _description = 'Wizard to add rma lines'
 
     @api.model
     def default_get(self, fields):
-        res = super(RmaAddinvoice, self).default_get(fields)
+        res = super(RmaAddPurchase, self).default_get(fields)
         rma_obj = self.env['rma.order']
         rma_id = self.env.context['active_ids'] or []
         active_model = self.env.context['active_model']
@@ -29,8 +29,8 @@ class RmaAddinvoice(models.TransientModel):
         rma = rma_obj.browse(rma_id)
         res['rma_id'] = rma.id
         res['partner_id'] = rma.partner_id.id
-        res['invoice_id'] = False
-        res['invoice_line_ids'] = False
+        res['purchase_id'] = False
+        res['purchase_line_ids'] = False
         return res
 
     rma_id = fields.Many2one('rma.order',
@@ -39,29 +39,28 @@ class RmaAddinvoice(models.TransientModel):
                               ondelete='cascade')
 
     partner_id = fields.Many2one(comodel_name='res.partner', string='Partner')
-    invoice_id = fields.Many2one(comodel_name='account.invoice',
-                                 string='Invoice')
-    invoice_line_ids = fields.Many2many('account.invoice.line',
-                                     'rma_add_invoice_add_line_rel',
-                                     'invoice_line_id', 'rma_add_invoice_id',
-                                     string='Invoice Lines')
+    purchase_id = fields.Many2one(comodel_name='purchase.order', string='Order')
+    purchase_line_ids = fields.Many2many('purchase.order.line',
+                                     'rma_add_purchase_add_line_rel',
+                                     'purchase_line_id', 'rma_add_purchase_id',
+                                     readonly=False,
+                                     string='Purcahse Order Lines')
 
-
-    def _prepare_rma_line_from_inv_line(self, line):
+    def _prepare_rma_line_from_po_line(self, line):
         operation = line.product_id.rma_operation_id and \
                     line.product_id.rma_operation_id.id or False
         if not operation:
             operation = line.product_id.categ_id.rma_operation_id and \
                         line.product_id.categ_id.rma_operation_id.id or False
         data = {
-            'invoice_line_id': line.id,
+            'purchase_order_line_id': line.id,
             'product_id': line.product_id.id,
             'name': line.name,
-            'origin': line.invoice_id.number,
-            'uom_id': line.uom_id.id,
+            'origin': line.order_id.name,
+            'uom_id': line.product_uom.id,
             'operation_id': operation,
-            'product_qty': line.quantity,
-            'price_unit': line.invoice_id.currency_id.compute(
+            'product_qty': line.product_qty,
+            'price_unit': line.currency_id.compute(
                 line.price_unit, line.currency_id, round=False),
             'rma_id': self.rma_id.id
         }
@@ -71,26 +70,26 @@ class RmaAddinvoice(models.TransientModel):
     def _get_rma_data(self):
         data = {
             'date_rma': fields.Datetime.now(),
-            'delivery_address_id': self.invoice_id.partner_id.id,
-            'invoice_address_id': self.invoice_id.partner_id.id
+            'delivery_address_id': self.purchase_id.partner_id.id,
+            'invoice_address_id': self.purchase_id.partner_id.id
         }
         return data
 
     @api.model
-    def _get_existing_invoice_lines(self):
-        existing_invoice_lines = []
+    def _get_existing_purchase_lines(self):
+        existing_purchase_lines = []
         for rma_line in self.rma_id.rma_line_ids:
-            existing_invoice_lines.append(rma_line.invoice_line_id)
-        return existing_invoice_lines
+            existing_purchase_lines.append(rma_line.purchase_order_line_id)
+        return existing_purchase_lines
 
     @api.multi
     def add_lines(self):
         rma_line_obj = self.env['rma.order.line']
-        existing_invoice_lines = self._get_existing_invoice_lines()
-        for line in self.invoice_line_ids:
+        existing_purchase_lines = self._get_existing_purchase_lines()
+        for line in self.purchase_line_ids:
             # Load a PO line only once
-            if line not in existing_invoice_lines:
-                data = self._prepare_rma_line_from_inv_line(line)
+            if line not in existing_purchase_lines:
+                data = self._prepare_rma_line_from_po_line(line)
                 rma_line_obj.create(data)
         rma = self.rma_id
         data_rma = self._get_rma_data()
--- rma_sale/wizards/rma_add_sale.py
+++ rma_sale/wizards/rma_add_sale.py
@@ -12,13 +12,13 @@ from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as DT_FORMAT
 import openerp.addons.decimal_precision as dp
 
 
-class RmaAddinvoice(models.TransientModel):
-    _name = 'rma_add_invoice'
+class RmaAddSale(models.TransientModel):
+    _name = 'rma_add_sale'
     _description = 'Wizard to add rma lines'
 
     @api.model
     def default_get(self, fields):
-        res = super(RmaAddinvoice, self).default_get(fields)
+        res = super(RmaAddSale, self).default_get(fields)
         rma_obj = self.env['rma.order']
         rma_id = self.env.context['active_ids'] or []
         active_model = self.env.context['active_model']
@@ -29,8 +29,8 @@ class RmaAddinvoice(models.TransientModel):
         rma = rma_obj.browse(rma_id)
         res['rma_id'] = rma.id
         res['partner_id'] = rma.partner_id.id
-        res['invoice_id'] = False
-        res['invoice_line_ids'] = False
+        res['sale_id'] = False
+        res['sale_line_ids'] = False
         return res
 
     rma_id = fields.Many2one('rma.order',
@@ -39,29 +39,29 @@ class RmaAddinvoice(models.TransientModel):
                               ondelete='cascade')
 
     partner_id = fields.Many2one(comodel_name='res.partner', string='Partner')
-    invoice_id = fields.Many2one(comodel_name='account.invoice',
-                                 string='Invoice')
-    invoice_line_ids = fields.Many2many('account.invoice.line',
-                                     'rma_add_invoice_add_line_rel',
-                                     'invoice_line_id', 'rma_add_invoice_id',
-                                     string='Invoice Lines')
+    sale_id = fields.Many2one(comodel_name='sale.order', string='Order')
+    sale_line_ids = fields.Many2many('sale.order.line',
+                                     'rma_add_sale_add_line_rel',
+                                     'sale_line_id', 'rma_add_sale_id',
+                                     readonly=False,
+                                     string='Sale Lines')
 
 
-    def _prepare_rma_line_from_inv_line(self, line):
+    def _prepare_rma_line_from_sale_order_line(self, line):
         operation = line.product_id.rma_operation_id and \
                     line.product_id.rma_operation_id.id or False
         if not operation:
             operation = line.product_id.categ_id.rma_operation_id and \
                         line.product_id.categ_id.rma_operation_id.id or False
         data = {
-            'invoice_line_id': line.id,
+            'sale_line_id': line.id,
             'product_id': line.product_id.id,
             'name': line.name,
-            'origin': line.invoice_id.number,
-            'uom_id': line.uom_id.id,
+            'origin': line.order_id.name,
+            'uom_id': line.product_uom.id,
             'operation_id': operation,
-            'product_qty': line.quantity,
-            'price_unit': line.invoice_id.currency_id.compute(
+            'product_qty': line.product_uom_qty,
+            'price_unit': line.currency_id.compute(
                 line.price_unit, line.currency_id, round=False),
             'rma_id': self.rma_id.id
         }
@@ -71,26 +71,26 @@ class RmaAddinvoice(models.TransientModel):
     def _get_rma_data(self):
         data = {
             'date_rma': fields.Datetime.now(),
-            'delivery_address_id': self.invoice_id.partner_id.id,
-            'invoice_address_id': self.invoice_id.partner_id.id
+            'delivery_address_id': self.sale_id.partner_id.id,
+            'invoice_address_id': self.sale_id.partner_id.id
         }
         return data
 
     @api.model
-    def _get_existing_invoice_lines(self):
-        existing_invoice_lines = []
+    def _get_existing_sale_lines(self):
+        existing_sale_lines = []
         for rma_line in self.rma_id.rma_line_ids:
-            existing_invoice_lines.append(rma_line.invoice_line_id)
-        return existing_invoice_lines
+            existing_sale_lines.append(rma_line.sale_line_id)
+        return existing_sale_lines
 
     @api.multi
     def add_lines(self):
         rma_line_obj = self.env['rma.order.line']
-        existing_invoice_lines = self._get_existing_invoice_lines()
-        for line in self.invoice_line_ids:
+        existing_sale_lines = self._get_existing_sale_lines()
+        for line in self.sale_line_ids:
             # Load a PO line only once
-            if line not in existing_invoice_lines:
-                data = self._prepare_rma_line_from_inv_line(line)
+            if line not in existing_sale_lines:
+                data = self._prepare_rma_line_from_sale_order_line(line)
                 rma_line_obj.create(data)
         rma = self.rma_id
         data_rma = self._get_rma_data()
