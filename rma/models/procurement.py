ace9d46f32405ac45c5094c3f4cf000c2d94b9f6
aheficent <ahenriquez@eficent.com>
2017-05-15 18:50:06 +0200

    [FIX] delivery address in procurement

--- rma/models/procurement.py
+++ rma/models/procurement.py
@@ -19,9 +19,9 @@ class ProcurementOrder(models.Model):
         if procurement.rma_line_id:
             line = procurement.rma_line_id
             res['rma_line_id'] = line.id
-            rma = line.rma_id.id
-            if rma.partner_address_id and rma.partner_address_id.id:
-                res['partner_id'] = rma.partner_address_id.id
+            rma = line.rma_id
+            if rma.delivery_address_id and rma.delivery_address_id.id:
+                res['partner_id'] = rma.delivery_address_id.id
             elif line.invoice_line_id.invoice_id.partner_id:
                 res['partner_id'] = line.invoice_id.partner_id.id
         return res
